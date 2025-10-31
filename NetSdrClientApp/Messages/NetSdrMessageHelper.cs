2f502d99a3a401b6ec9d36efaa6d6cd6594c46a9
Mykola-Kuzmenko <8121735@stud.kai.edu.ua>
2025-10-31 23:18:20 +0200

    Lab_5 Додати кілька архітектурних правил залежностей

--- NetSdrClientApp/Messages/NetSdrMessageHelper.cs
+++ NetSdrClientApp/Messages/NetSdrMessageHelper.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
+using NetSdrClientApp.Networking;
+
 
 namespace NetSdrClientApp.Messages
 {
     //TODO: analyze possible use of [StructLayout] for better performance and readability 
     public static class NetSdrMessageHelper
     {
+        
         private const short _maxMessageLength = 8191;
         private const short _maxDataItemMessageLength = 8194;
         private const short _msgHeaderLength = 2; //2 byte, 16 bit
@@ -157,5 +160,6 @@ namespace NetSdrClientApp.Messages
                 msgLength = _maxDataItemMessageLength;
             }
         }
+       
     }
 }
